3f3d78b8aadb179e5119c8b7b8ec2360b5ab5a53
Prem <ps09311@gmail.com>
2023-07-01 19:46:38 +0530

    Resume section added

---
+++ app.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    $("#hire-me-btn").hover(function () {
+
+        $(this).animate({ top: '8px' });
+    },
+        function () {
+            $(this).animate({ top: '10px' });
+        })
+
+      
+
+
+
+
+
+
+});
+
